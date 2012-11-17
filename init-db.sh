b8d546db6418d2b52efbbaf74e5099c80bfa5540
Yusaku Sako <yusaku@hortonworks.com>
2012-11-17 13:04:30 -0800

    Updated init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+service postgresql stop
 rm -rf /var/lib/pgsql/data
 sudo -u postgres initdb /var/lib/pgsql/data
 cp -f pg_hba.conf /var/lib/pgsql/data
 cp -f postgresql.conf /var/lib/pgsql/data
 sleep 5
-service postgresql restart
+service postgresql start
 sudo -u postgres psql -U postgres < $AMBARI_DIR/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
