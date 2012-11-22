c2868c5ced08cd19d5038fd438fb1e412bc2262e
Yusaku Sako <yusaku@hortonworks.com>
2012-11-21 16:46:44 -0800

    Added test API call to create multiple components

---
+++ test/create-components.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+curl -i -X POST -d '{
+"components":[
+{"ServiceComponentInfo" : { "component_name": "NAMENODE"} },
+{"ServiceComponentInfo" : { "component_name": "DATANODE"} }
+]
+}' http://localhost:8080/api/v1/clusters/mycluster/services?ServiceInfo/service_name=HDFS
