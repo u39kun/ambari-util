0ebd76e6949ba6ad5146f57e5ad30f1a37cc073b
Yusaku Sako <yusaku@hortonworks.com>
2012-11-21 16:36:33 -0800

    Attempt to create multiple services at the same time

--- test/create-multiple-services.sh
+++ test/create-multiple-services.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 curl -i -X POST -d '{
-"Clusters": {"version" : "HDP-1.2.0"},
 "services":[
 {"ServiceInfo" : { "service_name": "HDFS"} },
 {"ServiceInfo" : { "service_name": "MAPREDUCE"} }
 ]
-}' http://localhost:8080/api/v1/clusters/mycluster
+}' http://localhost:8080/api/v1/clusters?Clusters/cluster_name=mycluster
