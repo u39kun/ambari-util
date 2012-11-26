ce27bd0df772c9049cfa220cf26032be362ea781
Yusaku Sako <yusaku@hortonworks.com>
2012-11-26 11:00:56 -0800

    Modified init-db.sh to make it work with the new database creation script

--- init-db.sh
+++ init-db.sh
@@ -7,4 +7,7 @@ cp -f pg_hba.conf /var/lib/pgsql/data
 cp -f postgresql.conf /var/lib/pgsql/data
 sleep 5
 service postgresql start
-sudo -u postgres psql -U postgres < $AMBARI_DIR/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
+#sudo -u postgres psql -U postgres < $AMBARI_DIR/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
+cp -f $AMBARI_DIR/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql /tmp
+sleep 5
+sudo -u postgres psql -U postgres -f /tmp/Ambari-DDL-Postgres-CREATE.sql -v username="\"ambari-server\"" -v password="'bigdata'"
