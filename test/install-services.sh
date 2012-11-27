2dd8371c508e88f45173dd25c587881a961e6dd6
Yusaku Sako <yusaku@hortonworks.com>
2012-11-26 17:48:00 -0800

    Fixed install-services.sh to use PUT rather than POST

--- test/install-services.sh
+++ test/install-services.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -i -X POST -d '{"ServiceInfo": {"state": "INSTALLED" } }' http://localhost:8080/api/v1/clusters/mycluster/services?ServiceInfo/state=INIT
+curl -i -X PUT -d '{"ServiceInfo": {"state": "INSTALLED" } }' http://localhost:8080/api/v1/clusters/mycluster/services?ServiceInfo/state=INIT
                                              
