1b39d60ef96aa163671c09e87021b14aebcc7c8d
Yusaku Sako <yusaku@hortonworks.com>
2012-11-28 13:27:16 -0800

    Conform to the new directories for ambari-agent

--- agent.sh
+++ agent.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-rm -f /var/run/ambari/ambari-agent.pid
+mkdir -p /var/run/ambari-agent
+mkdir -p /var/log/ambari-agent
+mkdir -p /tmp/ambari-agent
+rm -f /var/run/ambari-agent/ambari-agent.pid
 rm -rf /tmp/ambari-agent/*
-rm -f /var/log/ambari/ambari-agent.log
+rm -f /var/log/ambari-agent/ambari-agent.log
 rm -rf /var/lib/ambari-agent/puppet/modules
 cp -R /root/ambari/ambari-agent/src/main/puppet/modules /var/lib/ambari-agent/puppet
 cd /root/ambari/ambari-agent/src/main/python/ambari_agent
