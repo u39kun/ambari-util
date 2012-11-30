aba1eec35e5d78ab873f27941356faf1c7a31465
Yusaku Sako <yusaku@hortonworks.com>
2012-11-30 14:27:34 -0800

    Added script to create test hosts

---
+++ test/create-test-hosts.sql
@@ -0,0 +1,5 @@
+insert into ambari.hosts (host_name, cpu_count, cpu_info, discovery_status, disks_info, host_attributes, last_registration_time, os_arch, os_info, os_type, rack_info, total_mem) values ('dev2.hortonworks.com', 1, '', '', '', '', 0, '', '', '', '', 1234);
+insert into ambari.hosts (host_name, cpu_count, cpu_info, discovery_status, disks_info, host_attributes, last_registration_time, os_arch, os_info, os_type, rack_info, total_mem) values ('dev3.hortonworks.com', 1, '', '', '', '', 0, '', '', '', '', 1234);
+insert into ambari.hosts (host_name, cpu_count, cpu_info, discovery_status, disks_info, host_attributes, last_registration_time, os_arch, os_info, os_type, rack_info, total_mem) values ('dev4.hortonworks.com', 1, '', '', '', '', 0, '', '', '', '', 1234);
+
+
