c62e2346658ece80262949e778e32d0a27809e5f
Yusaku Sako <yusaku@hortonworks.com>
2012-11-30 13:48:36 -0800

    Fixed aggregate service creation test script

--- test/create-multiple-services.sh
+++ test/create-multiple-services.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-curl -i -X POST -d '{
-"services":[
+curl -i -X POST -d '[
 {"ServiceInfo" : { "service_name": "HDFS"} },
 {"ServiceInfo" : { "service_name": "MAPREDUCE"} }
-]
-}' http://localhost:8080/api/v1/clusters?Clusters/cluster_name=mycluster
+]' http://localhost:8080/api/v1/clusters/mycluster/services
