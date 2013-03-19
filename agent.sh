843356d7a699f1c03056135013ccd853cb80da00
Yusaku Sako <yusaku@hortonworks.com>
2013-03-18 20:43:01 -0700

    Files updated to enable stack upgrade testing

--- agent.sh
+++ agent.sh
@@ -7,6 +7,13 @@ rm -f /var/run/ambari-agent/ambari-agent.pid
 rm -rf /tmp/ambari-agent/*
 rm -f /var/log/ambari-agent/ambari-agent.log
 rm -rf /var/lib/ambari-agent/puppet/modules
+rm -rf /var/lib/ambari-agent/upgrade_stack
 cp -R /root/ambari/ambari-agent/src/main/puppet/modules /var/lib/ambari-agent/puppet
+cp -R /root/ambari/ambari-agent/src/main/upgrade_stack /var/lib/ambari-agent
+
+# hack to enable upgrade testing until HDP 1.3 repo is ready 
+mv /var/lib/ambari-agent/upgrade_stack/HDP-1.2_HDP-1.3 /var/lib/ambari-agent/upgrade_stack/HDP-1.2_HDP-1.2
+
+# start the agent
 cd /root/ambari/ambari-agent/src/main/python/ambari_agent
 python main.py
--- server.sh
+++ server.sh
@@ -6,9 +6,15 @@ tar zxvf $AMBARI_DIR/ambari-server/target/ambari-server*.tar.gz
 cd $AMBARI_DIR/ambari-server/target/ambari-server*SNAPSHOT
 cp $AMBARI_UTIL_DIR/ambari.properties etc/ambari-server/conf
 cp $AMBARI_UTIL_DIR/log4j.properties etc/ambari-server/conf
+
 rm -rf web
 ln -s $AMBARI_DIR/ambari-web/public web
 # comment out the line above and modify/uncomment the line below to 
 # allow ambari-web development directly from the host
 # ln -s /host/ambari-web/public web
+
+# hack to enable upgrade testing until HDP 1.3 repo is ready
+cp /root/ambari-util/metainfo.xml var/lib/ambari-server/resources/stacks/HDP/1.2.2
+
+# start the server
 java -cp etc/ambari-server/conf/:lib/ambari-server/* org.apache.ambari.server.controller.AmbariServer 
