ffa89b7d807992b7946dc3e8a07ed3c731ad49ce
Yusaku Sako <yusaku@hortonworks.com>
2013-04-16 18:14:41 -0700

    Updated agent startup script to better match the rpm layout directories

--- agent.sh
+++ agent.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+# clean up
+rm -rf /var/run/ambari-agent
+rm -rf /var/log/ambari-agent
+rm -rf /var/lib/ambari-agent
+
+mkdir -p /var/ambari-agent
+mkdir -p /etc/ambari-agent/conf
 mkdir -p /var/run/ambari-agent
 mkdir -p /var/log/ambari-agent
-mkdir -p /tmp/ambari-agent
-rm -f /var/run/ambari-agent/ambari-agent.pid
-rm -rf /tmp/ambari-agent/*
-rm -f /var/log/ambari-agent/ambari-agent.log
-rm -rf /var/lib/ambari-agent/puppet/modules
-rm -rf /var/lib/ambari-agent/upgrade_stack
-cp -R /root/ambari/ambari-agent/src/main/puppet/modules /var/lib/ambari-agent/puppet
+mkdir -p /var/lib/ambari-agent/data
+mkdir -p /var/lib/ambari-agent/keys
+mkdir -p /tmp/HDP-artifacts
+
+# deploy
+cp -R /root/ambari/ambari-agent/src/main/puppet /var/lib/ambari-agent
 cp -R /root/ambari/ambari-agent/src/main/upgrade_stack /var/lib/ambari-agent
-cp /root/ambari-util/version /tmp/ambari-agent
+cp /root/ambari-util/version /var/lib/ambari-agent/data
+cp /root/jdk*.bin /tmp/HDP-artifacts
+
+# prep the agent ini file 
+cp /root/ambari/ambari-agent/conf/unix/ambari-agent.ini /etc/ambari-agent/conf
+sed 's/hostname=.*/hostname=dev01.hortonworks.com/' /etc/ambari-agent/conf/ambari-agent.ini > /etc/ambari-agent/conf/tmp.ini; mv /etc/ambari-agent/conf/tmp.ini /etc/ambari-agent/conf/ambari-agent.ini
 
 # start the agent
 cd /root/ambari/ambari-agent/src/main/python/ambari_agent
